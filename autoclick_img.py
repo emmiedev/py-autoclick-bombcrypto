dd7e43e56241660e02b5befb8963535885e790d2
tawatchaiinsree <tawatchaiinsree@gmail.com>
2022-01-21 09:59:01 +0700

    windows screen =shot

--- autoclick_img.py
+++ autoclick_img.py
@@ -5,8 +5,9 @@ from subprocess import call
 
 # def find_to_click(img, screen):
 def find_to_click(img):
-    call(["screencapture", "screenshot.png"])
-    image= cv2.imread("screenshot.png")
+    # call(["screencapture", "screenshot.png"])
+    # image = pyautogui.screenshot()
+    image= cv2.imread(pyautogui.screenshot())
     # image= cv2.imread(screen)
     gray= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
@@ -14,17 +15,16 @@ def find_to_click(img):
     result= cv2.matchTemplate(gray, template, cv2.TM_CCOEFF)
     min_val, max_val, min_loc, max_loc= cv2.minMaxLoc(result)
     height, width= template.shape[:2]
-    top_left= max_loc
-    Ax = top_left[0]
-    Ay = top_left[1]
+    Ax = max_loc[0]
+    Ay = max_loc[1]
     center = (((width/2)+(Ax))/2,(((height/2)+Ay))/2)
     pyautogui.click(center)
 
-
+browser_refresh_cord = (85,85) # ตำแหน่งปุ่มรีเฟรช
 while True:
     image_list = ['active_windows.png', 'connect_wallet.png', 'metamask_sign.png', 'show_heros.png', 'all_work.png', 'close_btn.png', 'treasure_hunt.png']
     pyautogui.click(1022, 565)
-    pyautogui.click(85, 85)
+    pyautogui.click(browser_refresh_cord)
     # find_to_click('refresh_btn.png')
     sleep(10)
     for index, img_file in enumerate(image_list):
